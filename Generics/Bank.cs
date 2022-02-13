6fb16ae95c7d774711230532d00a8b4cf0a6afbc
aidancer55 <bluebonnets77@gmail.com>
2022-02-13 21:00:50 +0300

    Добавьте файлы проекта.

---
+++ Generics/Bank.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Bank<T>
+    {
+        public T Num { get; set; }
+        public uint Balance { get; set; }
+        public string Name { get; set; }
+        public string Print()
+        {
+            return $"{Num} {Balance} {Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine();
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank<int> number1 = new Bank<int>();
+            number1.Input();
+            Console.WriteLine(number1.Print());
+            Console.WriteLine();
+            Bank<string> number2 = new Bank<string>();
+            number2.Input();
+            Console.WriteLine(number2.Print());
+            Console.ReadKey();
+        }
+    }
+}
